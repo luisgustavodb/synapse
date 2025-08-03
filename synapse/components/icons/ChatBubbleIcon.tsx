3ef2a8025a4be4bdd25ee1d15c966fb30d4c5535
luisgustavodb <109439578+luisgustavodb@users.noreply.github.com>
2025-08-03 18:34:29 -0300

    Delete synapse directory

--- synapse/components/icons/ChatBubbleIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const ChatBubbleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193l-3.722.537a5.25 5.25 0 0 1-5.064-4.418l-.252-1.007a25.25 25.25 0 0 0-5.138-4.225 2.12 2.12 0 0 1-1.02-1.884 5.254 5.254 0 0 1 3.232-4.689l3.612-1.445A5.25 5.25 0 0 1 15 5.25v4.286c0 .432.176.848.47 1.147Z" />
-  </svg>
-);
--- synapse/components/icons/GiftIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-export const GiftIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v18M3 9.75h18M5.25 3.75h13.5A2.25 2.25 0 0121 6v12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18V6a2.25 2.25 0 012.25-2.25z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 12c0 .966-.39 1.84-1.031 2.469a3.75 3.75 0 01-4.438 0M15.75 12c0 .966-.39 1.84-1.031 2.469a3.75 3.75 0 01-4.438 0" />
-  </svg>
-);
