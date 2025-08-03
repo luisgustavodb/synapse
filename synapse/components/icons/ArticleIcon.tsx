eda0db89563993169abc4c14a8b02e53d5df1374
luisgustavodb <109439578+luisgustavodb@users.noreply.github.com>
2025-08-03 18:44:01 -0300

    Add files via upload

---
+++ synapse/components/icons/ArticleIcon.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+export const ArticleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25h-8.25" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="m21.75 12.75-2.636 2.636a.563.563 0 0 1-.792 0l-2.636-2.636m4.062 4.062-2.636 2.636a.563.563 0 0 1-.792 0l-2.636-2.636m-1.772-1.772-2.636 2.636a.563.563 0 0 1-.792 0l-2.636-2.636" />
+    </svg>
+);
