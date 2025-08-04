20af687320ffe9d12bb1842638f77d59968953ee
luisgustavodb <109439578+luisgustavodb@users.noreply.github.com>
2025-08-04 01:01:16 -0300

    Delete synapse directory

--- synapse/components/icons/PuzzlePieceIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-export const PuzzlePieceIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.66.538-1.192 1.2-1.192h.013c.662 0 1.199.532 1.199 1.192v.013c0 .66-.538 1.192-1.2 1.192h-.013c-.662 0-1.199-.532-1.199-1.192v-.013zM4.5 15.75c0-.66.538-1.192 1.2-1.192h.013c.662 0 1.199.532 1.199 1.192v.013c0 .66-.538 1.192-1.2 1.192h-.013C5.038 16.942 4.5 16.41 4.5 15.75v-.013zM12.75 12a3 3 0 00-3-3H6.75a3 3 0 00-3 3v.013c0 .66.538 1.192 1.2 1.192h.013c.662 0 1.199-.532 1.199-1.192V12a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v.013c0 .66.538 1.192 1.2 1.192h.013c.662 0 1.199-.532 1.199-1.192V12z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 12c0 5.378-4.372 9.75-9.75 9.75S2.25 17.378 2.25 12 6.622 2.25 12 2.25c4.137 0 7.757 2.57 9.11 6.138" />
-  </svg>
-);
